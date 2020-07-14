9e462f720d89c8f04e9ec09a4598be140362b161
Proudust <proudust@gmail.com>
2020-07-14 15:26:15 +0900

    Adjust message box font size and letter spacing

--- www/js/main.js
+++ www/js/main.js
@@ -10,6 +10,9 @@ if (enableJpPatch) {
 
     // Disable "Bitmap Fonts" plugin
     $plugins[4].status = false;
+
+    // Replace "YEP_MessageCore" plugin parameters
+    $plugins[5].parameters["Font Size"] = "36"
 }
 
 PluginManager.setup($plugins);
@@ -27,6 +30,13 @@ window.onload = function () {
         var Bitmap_drawText = Bitmap.prototype.drawText;
         Bitmap.prototype.drawText = function (text, x, y, mW, l, align) {
             Bitmap_drawText.call(this, text, x, y + 4, mW, l, align);
+        };
+
+        // Adjust letter spacing
+        var Bitmap_measureTextWidth = Bitmap.prototype.measureTextWidth;
+        Bitmap.prototype.measureTextWidth = function (text) {
+            var width = Bitmap_measureTextWidth.call(this, text);
+            return width + 10;
         };
 
         // Change normal font color
